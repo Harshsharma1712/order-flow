7eb6ab9320dfc46e73f57742431033ccad9dcbbf
Harsh Sharma <sharmahs017@gmail.com>
2025-12-06 15:41:08 +0530

    add more shops route

--- app/routers/shop_router.py
+++ app/routers/shop_router.py
@@ -33,3 +33,66 @@ async def create_shop(
 
 
 # GET shops owned by current user
+@router.get("/my-shop", response_model=list[ShopResponse])
+async def get_my_shops(
+    current_user = Depends(require_shop_owner),
+    db: AsyncSession = Depends(get_db)
+):
+    result = await db.execute(
+        select(Shop).where(Shop.owner_id == current_user.id)
+    )
+
+    shops = result.scalars().all()
+
+    return shops
+
+# GET all shops
+@router.get("/all-shops")
+async def get_all_shops(
+    db: AsyncSession = Depends(get_db)
+):
+    result = await db.execute(select(Shop))
+    shops = result.scalars().all()
+
+    return shops
+
+
+
+# UPDATE shop (only owner)
+@router.put("/{shop_id}", response_model=ShopResponse)
+async def update_shop(
+    shop_id: int,
+    data: ShopUpdate,
+    current_user = Depends(require_shop_owner),
+    db: AsyncSession = Depends(get_db)
+):
+    # find shop
+    result = await db.execute(select(Shop).where(Shop.id == shop_id))
+
+    shop = result.scalar_one_or_none()
+
+    if not shop:
+        raise HTTPException(
+            status_code=400,
+            detail="Shop not found"
+        )    
+    
+    if shop.owner_id != current_user.id:
+        raise HTTPException(
+            status_code=403,
+            detail="You are not the owner of this shop"
+        )
+    
+    # update field
+    updated_data = data.model_dump(exclude_unset=True)
+
+    for key, value in updated_data.items():
+        setattr(shop, key, value)
+
+    await db.commit()
+    await db.refresh(shop)
+    return shop
+
+    
+
+
